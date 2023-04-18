0447827cb543dae1f4ed19f85c3dfddb42d6cbc4
Jun Wan <jun.wan@convertlab.com>
2023-04-18 19:06:00 +0800

    Split call to Azure API.

--- uploads/models.py
+++ uploads/models.py
@@ -6,10 +6,10 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Milvus
 import os
 
-os.environ["OPENAI_API_TYPE"] = "my-name"
-os.environ["OPENAI_API_BASE"] = "https://my-site.com/"
-os.environ["OPENAI_API_KEY"] = "my-secret"
-os.environ["OPENAI_API_VERSION"] = "my-version"
+os.environ["OPENAI_API_TYPE"] = "azure"
+os.environ["OPENAI_API_BASE"] = "https://your-azure-region.api.cognitive.microsoft.com/"
+os.environ["OPENAI_API_KEY"] = "your-api-key"
+os.environ["OPENAI_API_VERSION"] = "your-api-version"
 
 
 # Create your models here.
@@ -25,10 +25,10 @@ class Document(models.Model):
         loader = TextLoader(self.document.path)
         loader.encoding = "ISO8859-1"
         docs = loader.load()
-        text_splitter = CharacterTextSplitter(chunk_size=30000, chunk_overlap=0)
+        text_splitter = CharacterTextSplitter(chunk_size=10000, chunk_overlap=0)
         docs = text_splitter.split_documents(docs)
         if docs is not None and docs != []:
-            embeddings = OpenAIEmbeddings(chunk_size=1)
+            embeddings = OpenAIEmbeddings(chunk_size=10)
             vector_db = Milvus.from_documents(
                 docs,
                 embeddings,
